08f6a62bb7921675cd74d7f88fd3d3a5590e6d61
AilsyaRachma03 <63080602+AilsyaRachma03@users.noreply.github.com>
2021-06-15 10:06:12 +0700

    nambah di dbnya by rachma canteekkkk

--- pengaduan.sql
+++ pengaduan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Jun 2021 pada 09.00
+-- Waktu pembuatan: 15 Jun 2021 pada 04.52
 -- Versi server: 10.4.16-MariaDB
 -- Versi PHP: 7.4.12
 
@@ -41,18 +41,12 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `email`, `password`, `nama`, `no_telp`, `bidang`) VALUES
-(1, '0', 'rachmaailsya@gmail.com', '$2y$10$/kXuAAlMFUM4p6PyiMcrhO7zbJysLaXQJnBBdy', 'Rachma Ailsya', '085325221893'),
-(2, '0', 'RachmaAilsya', '$2y$10$WEWHOSs9EMGQbl9Z13LbnOBlv5bqG667bIhvj/', 'Rachma Ailsya', '085325221893'),
-(3, '0', '$2y$10$e/IFeuTPXWwXqTPuWoWBuOmVxIcllHbAHgryPHvuGSIcf9d2oSQLy', 'Rachma', '085325221893', 'Human Resource Director'),
-(4, '0', '$2y$10$LX5rENrN7bQ2nj7gCHX24u.Mn4y0MnNeGbnkXuyDsiXdX/kf3mFFK', 'Rachma', '085325221893', 'Human Resource Director'),
 (5, 'rachma@yahoo.com', '$2y$10$HU39.iS09TwJYGu8sGGHAuZlTI1LAjcXqDRbv0MizDSoOk4ZWU1qi', 'Rachma', '085325221893', 'Human Resource Director'),
 (6, 'ailsyar@yahoo.com', '$2y$10$/mASld5I4bTgKwvBcWXm5erunWv2OTT683LT7KljbrHmUyo6beRc.', 'Rachma Ailsya', '085325221893', 'Manajer Operation'),
 (7, 'azril@gmail.com', '$2y$10$KXpRdsc8TGkLGe6QNax9JOXEc/XtgBHsMJ4dRzqB17yJDGnhxtBkG', 'Azril Ihza Rahman', '085325221893', 'Manajer Operation'),
-(8, 'BaiduriZahra123', '$2y$10$LugFaxzm9VvGodcD645YEeAKbCy5DcwlX5RlGvPY7MuIe4dC2Ha2W', '', '', ''),
 (9, 'ailsyar@yahoo.com', '$2y$10$ixIj3eYQBTtNL8yRFs9Ciua3J407k.X7QVvnJpRABzHmHwTasSnGK', 'Rachma Ailsya', '085325221893', 'Human Resource Director'),
-(10, 'aaa', '$2y$10$Tt/KqrEyMYIXrM/JyAMn8uEZUjcaugviPgWpOKUmuneq5AjN15.fO', 'Rachma', '085325221893', 'Human Resource Director'),
-(11, 'ailsyar@yahoo.com', '$2y$10$Tm62e1czo8uWI4q5LCEcc.mhjqfwOKBi/mxjq5F6kkIlI.JWE8whW', 'Rachma', '085325221893', 'Human Resource Director'),
-(12, 'Icha', '$2y$10$F5soQJJcuxtlCqzpPnoedu/4UM.TRgLaMksbgxE0DzbRPWKJ.yYji', 'Icha', '085325221893', 'Human Resource Director');
+(12, 'Icha', '$2y$10$F5soQJJcuxtlCqzpPnoedu/4UM.TRgLaMksbgxE0DzbRPWKJ.yYji', 'Icha', '085325221893', 'Human Resource Director'),
+(13, 'aku', '$2y$10$mCNoJp3SJuaOEGStFmeYcO6o60tkoFL/4oN4L2mSSdCaBxgHlr/zq', 'Rachma', '085325221893', 'Human Resource Director');
 
 -- --------------------------------------------------------
 
@@ -77,9 +71,8 @@ CREATE TABLE `aduan` (
 --
 
 INSERT INTO `aduan` (`id`, `nama`, `email`, `alamat`, `keluhan`, `telp`, `kelamin`, `jenis`, `file`) VALUES
-(1, 'q', 'q', 'q', 'q', 'q', 'q', 'Pemerkosaan', ''),
-(2, 'Icha', 'Clarissa@gmail.com', 'Jember', 'edewfre', '093', 'Perempuan', 'Pemerkosaan', ''),
-(3, 'Indah', 'nurindahfidiawati22@gmail.com', 'Jember', 'ww', '0897', 'Perempuan', 'Pelecehan, KDR', ''),
+(2, 'Nyai Icha', 'Clarissa@gmail.com', 'Jember', 'saya dijambret', '085', 'Perempuan', 'Pemerkosaan', ''),
+(3, 'Nur Indah Fidia Wati', 'nurindahfidiawati22@gmail.com', 'Jember', 'ww', '0897', 'Perempuan', 'Pelecehan, KDR', ''),
 (4, 'Indah', 'nurindahfidiawati22@gmail.com', 'Jember', 'ww', '0897', 'Perempuan', 'Pelecehan, KDR', ''),
 (5, 'Rachma Ailsya', 'nurindahfidiawati22@gmail.com', 'q', 'www', '2', 'Perempuan', 'Kekerasan', ''),
 (6, 'Rachma', 'nurindahfidiawati22@gmail.com', 'ww', 'edewfre', '2', 'Perempuan', 'Pemerkosaan', ''),
@@ -104,7 +97,8 @@ INSERT INTO `aduan` (`id`, `nama`, `email`, `alamat`, `keluhan`, `telp`, `kelami
 (25, 'dwqdqw', 'dwqdwqd', 'wqdwqdqw', 'dqwdwqd', 'wqdqwdqwdwqdqwd', 'wqdwqdwqdwqd', 'Pemerkosaan', ''),
 (26, 'dwqdqw', 'dwqdwqd', 'wqdwqdqw', 'dqwdwqd', 'wqdqwdqwdwqdqwd', 'wqdwqdwqdwqd', 'Pemerkosaan', ''),
 (27, 'Adinda', 'nurindahfidiawati22@gmail.com', 'Jember', 'ww', '2', '2', 'KDRT', 'BG ZOOM ITEC.png'),
-(28, 'we', 'werwe', 'ewfew', 'fwsdfw', 'fsdfws', 'fdsfs', 'Pemerkosaan', 'BG ZOOM ITEC.png');
+(29, '5g', 'tgrh', 'yth', 'yh', '5yh', 'yh', 'Pemerkosaan, P', 'Philosoppy.png'),
+(30, 'dg', 'erg', 'rt', 'ert', 'ert', 'ret', 'Pemerkosaan, P', 'Screenshot (33).png');
 
 -- --------------------------------------------------------
 
@@ -125,8 +119,38 @@ CREATE TABLE `pengguna` (
 
 INSERT INTO `pengguna` (`id`, `email`, `password`, `nama`) VALUES
 (1, 'azril@gmail.com', '$2y$10$yyblG/BLS2VBJ3xUvXuO1eVvNpe/lM9XC4DMdO5HtmIkHSTF0P2ZC', 'Azril Ihza Rachman'),
-(2, 'Adinda@gmail.com', '$2y$10$PKtaGjrp0TbfQS516mrYsOkgsctr/HEDOoNezQmZEfIkIqn00D/4q', 'Adinda'),
-(3, 'vivi', '$2y$10$OSogu1D/HURm3aH8YZeLPexsgJ.NtpDmGTzkBUgsnh8iZSEsuM.r.', 'Ichaa');
+(4, 'ailsyar@yahoo.com', '$2y$10$MavqaRZRC4NpeaonE5lSMeKkUIIz85cd2zYgRX.n2HWHZAW74Iv5a', 'Rachma Ailsya'),
+(6, 'vivi@gmail.com', '$2y$10$fUorl3AFyL9AdFOSinmhn.DFeS7FgY5efhVaYVEAMhy02vyeq4xS6', 'vivi'),
+(7, 'maman', '$2y$10$7utGB7tWbSkY9fAQqXjXx.osMslCU.aPezx40CKgXgAL9zuft4Bhq', 'maman'),
+(8, 'vivisayangihaq', '$2y$10$ftIW7TkUAOq8jMFbwEFWNucFJ6Sl0BDid9x73xVUGHPFOgvJ2sZRO', 'viviihaq'),
+(11, 'Indah', '$2y$10$GLgRSICx73lBEmv/02gKO.k8LfZatXRI0hss3nqfRToepkXGyQTG2', 'Indah'),
+(12, 'Dwijannah', '$2y$10$SzxWvDrd.PN7FqZocIwc.OSgqs/SW6vWXmtnc4gdr6sluHkD2Ga0a', 'Dwi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `isipesan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pesan`
+--
+
+INSERT INTO `pesan` (`id`, `nama`, `email`, `isipesan`) VALUES
+(1, 'Rachma Ailsya', 'azril@gmail.com', ''),
+(5, 'Azril Ihza Rahman', 'ailsyar@yahoo.com', ''),
+(6, 'Rachma Ailsya', 'rachmaailsya9@gmail.com', ''),
+(7, 'Rachma Ailsya', 'rachmaailsya9@gmail.com', ''),
+(8, 'Rachma', 'azril@gmail.com', 't'),
+(9, 'Rachma Ailsya', 'azril@gmail.com', 't'),
+(10, 'Azril Ihza Rahman', 'rachmaailsya9@gmail.com', 'k');
 
 --
 -- Indexes for dumped tables
@@ -151,6 +175,12 @@ ALTER TABLE `pengguna`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -158,19 +188,25 @@ ALTER TABLE `pengguna`
 -- AUTO_INCREMENT untuk tabel `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `aduan`
 --
 ALTER TABLE `aduan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
